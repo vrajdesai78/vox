d5b98db427050ebbc20d6861ff467a0941677ac3
Not-Sarthak <notsarthakshah@gmail.com>
2024-10-08 12:43:08 +0530

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,33 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { PrivyProvider } from "@privy-io/react-auth";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+
+        <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicons/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicons/manifest.json" />
+
+        <title>Base Hackathon Project</title>
+        <meta name="description" content="Privy Auth Starter" />
+      </Head>
+      <PrivyProvider
+        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
+        config={{
+          embeddedWallets: {
+            createOnLogin: "all-users",
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </PrivyProvider>
+    </>
+  );
+}
+
+export default MyApp;
