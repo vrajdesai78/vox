cb85012f253859960ceaf17703ad5dd16566f58b
vrajdesai78 <vrajdesai78@gmail.com>
2024-10-11 14:43:54 +0530

    fixed build error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,17 +1,16 @@
-import Image from 'next/image';
-import {useRouter} from 'next/router';
-import {Fragment} from 'react';
-import {Disclosure, Menu, Transition} from '@headlessui/react';
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
 import {
   ChevronDownIcon,
   Bars3Icon,
   XMarkIcon,
   InformationCircleIcon,
-} from '@heroicons/react/24/outline';
-import {Logo} from './logo';
+} from "@heroicons/react/24/outline";
 
 function classNames(...classes: Array<string | boolean>): string {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
 /**
@@ -20,7 +19,7 @@ function classNames(...classes: Array<string | boolean>): string {
  * fragments, query params, or trailing slashes
  */
 const extractTabFromPath = (path: string) => {
-  return path.split('/').pop() as string;
+  return path.split("/").pop() as string;
 };
 
 export type NavbarItem = {
@@ -35,15 +34,15 @@ type NavbarProps = {
   items: Array<NavbarItem>;
 };
 
-export default function Navbar({items, accountId, appName}: NavbarProps) {
+export default function Navbar({ items, accountId, appName }: NavbarProps) {
   const router = useRouter();
   const resourceId = router.query.id;
   const selected = extractTabFromPath(router.pathname);
 
   const selectedItemClass =
-    'hover:cursor-pointer rounded-full bg-gray-900 px-3 py-2 text-lg font-medium text-white';
+    "hover:cursor-pointer rounded-full bg-gray-900 px-3 py-2 text-lg font-medium text-white";
   const unselectedItemClass =
-    'hover:cursor-pointer rounded-full px-3 py-2 text-lg font-medium text-gray-300 hover:bg-gray-700 hover:text-white';
+    "hover:cursor-pointer rounded-full px-3 py-2 text-lg font-medium text-gray-300 hover:bg-gray-700 hover:text-white";
 
   // Navigate to a resource sub-page:
   // /apps/:appId/settings
@@ -53,22 +52,18 @@ export default function Navbar({items, accountId, appName}: NavbarProps) {
   };
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      {({open}) => (
+    <Disclosure as='nav' className='bg-gray-800'>
+      {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-4">
-            <div className="flex h-16 items-center justify-between">
-              <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <div className="block h-8 w-auto lg:hidden mb-2">
-                    <Logo />
-                  </div>
-                  <div className="hidden h-8 w-auto lg:block mb-2 hover:cursor-pointer">
-                    <Logo />
-                  </div>
+          <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-4'>
+            <div className='flex h-16 items-center justify-between'>
+              <div className='flex items-center'>
+                <div className='flex-shrink-0'>
+                  <div className='block h-8 w-auto lg:hidden mb-2'></div>
+                  <div className='hidden h-8 w-auto lg:block mb-2 hover:cursor-pointer'></div>
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
+                <div className='hidden sm:ml-6 sm:block'>
+                  <div className='flex space-x-4'>
                     {items ? (
                       items.map((item) => {
                         return (
@@ -78,7 +73,9 @@ export default function Navbar({items, accountId, appName}: NavbarProps) {
                               navigateTo(item);
                             }}
                             className={
-                              selected === item.id ? selectedItemClass : unselectedItemClass
+                              selected === item.id
+                                ? selectedItemClass
+                                : unselectedItemClass
                             }
                           >
                             {item.name}
@@ -91,50 +88,56 @@ export default function Navbar({items, accountId, appName}: NavbarProps) {
                   </div>
                 </div>
               </div>
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex items-center">
+              <div className='hidden sm:ml-6 sm:block'>
+                <div className='flex items-center'>
                   <button
-                    type="button"
-                    className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white"
+                    type='button'
+                    className='rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white'
                   >
-                    <InformationCircleIcon className="h-6 w-6" aria-hidden="true" />
+                    <InformationCircleIcon
+                      className='h-6 w-6'
+                      aria-hidden='true'
+                    />
                   </button>
-                  <p className="text-white">{appName}</p>
+                  <p className='text-white'>{appName}</p>
 
                   {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-3">
-                    <div className="flex bg-gray-800 rounded-full items-center hover:ring-white hover:ring-2 hover:ring-offset-2 hover:ring-offset-gray-800 hover:outline-none hover:cursor-pointer">
-                      <Menu.Button className="flex rounded-full text-sm">
-                        <span className="sr-only">Open user menu</span>
-                        <div className="h-8 w-8 rounded-full">
+                  <Menu as='div' className='relative ml-3'>
+                    <div className='flex bg-gray-800 rounded-full items-center hover:ring-white hover:ring-2 hover:ring-offset-2 hover:ring-offset-gray-800 hover:outline-none hover:cursor-pointer'>
+                      <Menu.Button className='flex rounded-full text-sm'>
+                        <span className='sr-only'>Open user menu</span>
+                        <div className='h-8 w-8 rounded-full'>
                           <Image
-                            className="h-8 w-8 rounded-full"
-                            src="/images/avatar.png"
-                            alt="avatar placeholder"
-                            height="32px"
-                            width="32px"
+                            className='h-8 w-8 rounded-full'
+                            src='/images/avatar.png'
+                            alt='avatar placeholder'
+                            height='32'
+                            width='32'
                           />
                         </div>
                       </Menu.Button>
-                      <ChevronDownIcon className="ml-1 h-4 w-4 text-white" aria-hidden="true" />
+                      <ChevronDownIcon
+                        className='ml-1 h-4 w-4 text-white'
+                        aria-hidden='true'
+                      />
                     </div>
                     <Transition
                       as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
+                      enter='transition ease-out duration-100'
+                      enterFrom='transform opacity-0 scale-95'
+                      enterTo='transform opacity-100 scale-100'
+                      leave='transition ease-in duration-75'
+                      leaveFrom='transform opacity-100 scale-100'
+                      leaveTo='transform opacity-0 scale-95'
                     >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
                         <Menu.Item>
-                          {({active}) => (
+                          {({ active }) => (
                             <a
                               href={`/accounts/${accountId}`}
                               className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700',
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
                               )}
                             >
                               Your account
@@ -142,12 +145,12 @@ export default function Navbar({items, accountId, appName}: NavbarProps) {
                           )}
                         </Menu.Item>
                         <Menu.Item>
-                          {({active}) => (
+                          {({ active }) => (
                             <a
-                              href="#"
+                              href='#'
                               className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700',
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
                               )}
                             >
                               Settings
@@ -155,12 +158,12 @@ export default function Navbar({items, accountId, appName}: NavbarProps) {
                           )}
                         </Menu.Item>
                         <Menu.Item>
-                          {({active}) => (
+                          {({ active }) => (
                             <a
-                              href="#"
+                              href='#'
                               className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700',
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
                               )}
                             >
                               Sign out
@@ -172,14 +175,14 @@ export default function Navbar({items, accountId, appName}: NavbarProps) {
                   </Menu>
                 </div>
               </div>
-              <div className="-mr-2 flex sm:hidden">
+              <div className='-mr-2 flex sm:hidden'>
                 {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
+                <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                  <span className='sr-only'>Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
                   )}
                 </Disclosure.Button>
               </div>
